bcaa2db35bddac45fd1bd8ff14a445198e2d8a1b
TomasLonnblad <tomaslonnblad@gmail.com>
2025-08-28 08:03:51 +0200

    get great

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+
+
+
+export default function Home() {
+
+
+return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-20 p-24 bg-neutral-900 text-neutral-100">
+<div className="relative">
+<button className="relative bg-neutral-700 px-3 py-2 rounded-md hover:bg-neutral-800 transition-colors duration-200 z-10">
+XXXXXXXXXXXXXXXXXXXX
+</button>
+<div 
+className="absolute -inset-[1px] rounded-md"
+style={{
+  background:
+"conic-gradient(#ff4545, #00ff99, #006aff, #ff0095, #ff4545"
+}}
+/>
+</div>
+</main>
+)}
